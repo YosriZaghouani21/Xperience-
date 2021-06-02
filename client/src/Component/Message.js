4019f1e4c6dc4da942634f00a03f1c8f121e25ab
Yosrii <zaghouani.yosri@gmail.com>
2021-06-02 16:56:06 +0100

    SMS Fix issue

--- client/src/Component/Message.js
+++ client/src/Component/Message.js
@@ -1,54 +1,54 @@
-import React from 'react';
-const numberInput = document.getElementById('number'),
-  textInput = document.getElementById('msg'),
-  scheduleSelect = document.getElementById('schedule'),
-  button = document.getElementById('button'),
-  response = document.querySelector('.response');
-
-// 
-
-// const socket = io();
-// socket.on('smsStatus', function (data) {
-//   if (data.error) {
-//     response.innerHTML = '<h5>Text message sent to ' + data.error + '</h5>';
-//   } else {
-//     response.innerHTML = '<h5>Text message sent to ' + data.number + '</h5>';
-//   }
-// });
-
-let timeOut;
-const getTimeSchedule = ({time, number, text}) => {
-  if (timeOut) clearTimeout(timeOut);
-  timeOut = setTimeout(() => {
-    fetchServer({number, text});
-  }, time * 60 * 1000);
-};
-
-const fetchServer = ({number, text}) => {
-  console.log('send');
-  fetch('/', {
-    method: 'post',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify({number, text}),
-  })
-    .then(function (res) {
-      console.log(res);
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
-};
-
-function send() {
-  const number = numberInput.value.replace(/\D/g, '');
-  const text = textInput.value;
-  const time = parseInt(scheduleSelect.value, 10);
-  getTimeSchedule({number, text, time});
-}
-
+import React, {useEffect} from 'react';
 const Message = () => {
+  let button;
+  let numberInput;
+  let textInput;
+  let scheduleSelect;
+  let response;
+  let number;
+  let text;
+  let time;
+
+  let timeOut;
+  const getTimeSchedule = ({time, number, text}) => {
+    if (timeOut) clearTimeout(timeOut);
+    timeOut = setTimeout(() => {
+      fetchServer({number, text});
+    }, time * 60 * 1000);
+  };
+
+  const fetchServer = ({number, text}) => {
+    console.log('send');
+    fetch('/', {
+      method: 'post',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({number, text}),
+    })
+      .then(function (res) {
+        console.log(res);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  };
+
+  function send() {
+    number = numberInput.value.replace(/\D/g, '');
+    text = textInput.value;
+    time = parseInt(scheduleSelect.value, 10);
+    getTimeSchedule({number, text, time});
+  }
+
+  useEffect(() => {
+    button = document.getElementById('button');
+    button.addEventListener('click', send, 'false');
+    numberInput = document.getElementById('number');
+    textInput = document.getElementById('msg');
+    scheduleSelect = document.getElementById('schedule');
+    response = document.querySelector('.response');
+  });
   return (
     <div>
       {' '}
