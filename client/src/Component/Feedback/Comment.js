4519ec154b675859083904db69ab936aa6d15361
Yosrii <zaghouani.yosri@gmail.com>
2021-06-17 15:31:31 +0100

    update check

--- client/src/Component/Feedback/Comment.js
+++ client/src/Component/Feedback/Comment.js
@@ -2,16 +2,13 @@ import React, {useState} from 'react';
 import {comment, deleteComment, getExperienceDetails} from '../../JS/actions';
 import {useDispatch, useSelector} from 'react-redux';
 import {Button} from 'reactstrap';
-// import {set} from 'mongoose';
 var dateFormat = require('dateformat');
 var now = new Date();
 const Comment = () => {
   const [newComment, setNewComment] = useState('');
   const experience = useSelector(state => state.experiencesReducers.experience);
-  // console.log(experience.experience);
   const user = useSelector(state => state.userReducer.user);
 
-  // const comments = useSelector(state => state.experiencesReducers.experience.comments);
   const dispatch = useDispatch();
   const addCom = e => {
     e.preventDefault();
@@ -44,65 +41,61 @@ const Comment = () => {
               {experience &&
                 experience.experience &&
                 experience.experience.comments &&
-                experience.experience.comments
-                  // .filter(
-                  //   comment => experience.comments.indexOf(comment) !== experience.comments.length
-                  // )
-                  .map(comment => (
-                    <div
-                      style={{
-                        backgroundColor: '#F0F0F0',
-                        margin: '10px 50px 5px 50px',
-                        borderLeft: 'solid rgb(0, 249, 255)',
-                      }}
-                    >
-                      <div className="container">
-                        <span class="avatar avatar-lg rounded-circle">
-                          <img alt="..." src={comment.postedBy.photo} />
-                        </span>
-                        <div className="row">
-                          <p
-                            className="col-10 "
-                            style={{
-                              fontSize: 'small',
-                              fontStyle: 'italic',
-                            }}
-                          >
-                            {comment.postedBy.name} :
-                          </p>
+                experience.experience.comments.map(comment => (
+                  <div
+                    style={{
+                      backgroundColor: '#F0F0F0',
+                      margin: '10px 50px 5px 50px',
+                      borderLeft: 'solid rgb(0, 249, 255)',
+                    }}
+                  >
+                    <div className="container">
+                      <span class="avatar avatar-lg rounded-circle">
+                        <img alt="..." src={comment.postedBy.photo} />
+                      </span>
+                      <div className="row">
+                        <p
+                          className="col-10 "
+                          style={{
+                            fontSize: 'small',
+                            fontStyle: 'italic',
+                          }}
+                        >
+                          {comment.postedBy.name} :
+                        </p>
 
-                          <p
-                            className="col-2"
-                            style={{
-                              color: 'grey',
-                              fontStyle: 'italic',
-                              fontSize: 'small',
-                            }}
-                          >
-                            {dateFormat(comment.date, 'mediumDate')}{' '}
-                            {dateFormat(comment.date, 'shortTime')}
-                            {user._id == comment.postedBy._id ? (
-                              <Button
-                                outline
-                                color="info"
-                                size="sm"
-                                onClick={e => {
-                                  e.preventDefault();
-                                  dispatch(deleteComment(experience.experience._id, comment._id));
-                                }}
-                              >
-                                DELETE
-                              </Button>
-                            ) : (
-                              ''
-                            )}{' '}
-                          </p>
-                        </div>
+                        <p
+                          className="col-2"
+                          style={{
+                            color: 'grey',
+                            fontStyle: 'italic',
+                            fontSize: 'small',
+                          }}
+                        >
+                          {dateFormat(comment.date, 'mediumDate')}{' '}
+                          {dateFormat(comment.date, 'shortTime')}
+                          {user._id == comment.postedBy._id ? (
+                            <Button
+                              outline
+                              color="info"
+                              size="sm"
+                              onClick={e => {
+                                e.preventDefault();
+                                dispatch(deleteComment(experience.experience._id, comment._id));
+                              }}
+                            >
+                              DELETE
+                            </Button>
+                          ) : (
+                            ''
+                          )}{' '}
+                        </p>
                       </div>
-
-                      <div style={{marginLeft: '20px'}}>{comment.text}</div>
                     </div>
-                  ))}
+
+                    <div style={{marginLeft: '20px'}}>{comment.text}</div>
+                  </div>
+                ))}
             </div>
           </form>
         </div>
